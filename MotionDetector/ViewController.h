cbbecfc39d34b65fb3d8f7c2969f74b34d5960b3
Tomas Kamarauskas <tomas@kamatom.com>
2015-04-28 10:27:29 +0200

    Initial Commit

---
+++ MotionDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MotionDetector
+//
+//  Created by Tomas Kamarauskas on 28/04/15.
+//  Copyright (c) 2015 EEVOL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MotionDetector/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MotionDetector
+//
+//  Created by Tomas Kamarauskas on 28/04/15.
+//  Copyright (c) 2015 EEVOL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
